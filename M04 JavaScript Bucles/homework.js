dbfe17760f1d32a7cdd1a1057e0ed4c284c90159
Sergio Ugarte <sergiouga85@gmail.com>
2023-08-08 14:44:53 -0300

    tercer commit

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -47,8 +47,6 @@ function conection(status) {
    else{
       return "Offline"
    }
-
-
 }
 
 function saludo(idioma) {
@@ -58,6 +56,20 @@ function saludo(idioma) {
    // Si "idioma" es "ingles", devuelve "Hello!".
    // Si "idioma" no es ninguno de los anteriores o es `undefined` devuelve "Hola!".
    // Tu código:
+   if(idioma === 'aleman'){
+      return 'Guten Tag!'
+   }
+   else if(idioma === 'mandarin'){
+      return 'Ni Hao!'
+   }
+   else if(idioma === 'ingles'){
+      return 'Hello!'
+   }
+   else{
+      return 'Hola!'
+   }
+
+
 }
 
 function colors(color) {
@@ -69,18 +81,41 @@ function colors(color) {
    // Si no es ninguno de esos colores           --> "Color not found".
    // IMPORTANTE: utilizar el statement SWITCH.
    // Tu código:
+   switch(color){
+      case 'blue':
+      return 'This is blue'
+      case 'red':
+      return 'This is red'
+      case 'green':
+      return 'This is green'
+      case 'orange':
+      return 'This is orange'
+      default:
+         return 'Color not found'
+   }
 }
 
 function esDiezOCinco(num) {
    // Retornar true si "num" es 10 o 5.
    // De lo contrario, retornar false.
    // Tu código:
+   if(num ==10 ||num == 5 ){
+      return true
+   }else{
+      return false
+   }
 }
 
 function estaEnRango(num) {
    // Retornar true si "num" es menor que 50 y mayor que 20.
    // De lo contrario, retornar false.
    // Tu código:
+   if(num<50 && num>20){
+      return true
+   }else{
+      return false
+   }
+   
 }
 
 function esEntero(num) {
@@ -90,6 +125,11 @@ function esEntero(num) {
    // Ejemplo: (-10) ---> true
    // De lo contrario, retorna false.
    // Tu código:
+   if(Number.isInteger(num)){
+      return true
+   }else{
+   return false
+   }
 }
 
 function fizzBuzz(num) {
@@ -98,7 +138,22 @@ function fizzBuzz(num) {
    // Si "num" es divisible entre 3 y 5 (ambos), retorna "fizzbuzz".
    // De lo contrario, retorna false.
    // Tu código:
+   
+   if(num%3 == 0 && num%5== 0){
+      return "fizzbuzz";
+   }
+   else if (num %3 == 0) {
+      return "fizz";
+    }
+    else if(num%5 == 0){
+      return "buzz";
+    }
+    else{
+      return false
+    }
+
 }
+
 
 function operadoresLogicos(num1, num2, num3) {
    // La función recibe tres números distintos.
@@ -108,6 +163,23 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
+   if(num1 > num2 && num1 > num3 && num1>0){
+      return "Numero 1 es mayor y positivo"
+   }
+   else if(num1<0 || num2<0 || num3<0){
+      return "Hay negativos"
+   }
+   else if(num3>num1 && num3>num2){
+      num3=num3+1
+      return num3
+   }
+   else if(num1==0 && num2==0 && num3==0){
+      return "Error"
+   }
+   else{
+      return false
+   }
+
 }
 
 function esPrimo(num) {
@@ -117,18 +189,42 @@ function esPrimo(num) {
    // [Pista 2]: puedes resolverlo utilizando un `bucle for`.
    // [Nota]: los números negativos, 0 y 1 NO son números primos.
    // Tu código:
+
+   if (num <= 1) {
+      return false; // Los números negativos, 0 y 1 no son primos.
+    }
+    
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false; // Si num es divisible por algún número entre 2 y su raíz cuadrada, no es primo.
+      }
+    }
+    
+    return true; // Si no se encontró ningún divisor, num es primo.
 }
 
 function esVerdadero(valor) {
    // Si "valor" es verdadero retornar "Soy verdadero".
    // Caso contrario, retornar "Soy falso".
    // Tu código:
+   if(valor == true){
+      return "Soy verdadero"
+   }
+   else{
+      return "Soy falso"
+   }
 }
 
 function tieneTresDigitos(num) {
    // Si el número recibido tiene tres dígitos retornar true.
    // Caso contrario, retornar false.
    // Tu código:
+   if(num > 99 && num <1000){
+      return true
+   }
+   else{
+      return false
+   }
 }
 
 function doWhile(num) {
@@ -136,6 +232,16 @@ function doWhile(num) {
    // Retornar el valor final.
    // Utilizar el bucle Do-While.
    // Tu código:
+
+   let i=0;
+
+   do{
+      num=num+5;
+      i++;
+   }
+   while(i<8)
+
+   return num
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- M06 JavaScript Objetos/homework.js
+++ M06 JavaScript Objetos/homework.js
@@ -7,13 +7,16 @@ function crearGato(nombre, edad) {
    // La propiedad "meow" será una función que retorne el string: "Meow!".
    // Retornar el objeto.
    // Tu código:
+   const nuevoObjeto= {nombre:nombre, edad:edad, meow: function(){ return 'Meow!'}}
+   return nuevoObjeto
 }
-
 function nuevoUsuario(nombre, email, password) {
    // Debes crear un nuevo objeto.
    // Este debe tener las propiedades: "nombre", "email" y "password" con sus respectivos valores.
    // Retornar el objeto.
    // Tu código:
+   const nuevoObjeto={nombre:nombre, email:email, password:password}
+   return nuevoObjeto
 }
 
 function agregarPropiedad(objeto, propiedad) {
@@ -22,6 +25,8 @@ function agregarPropiedad(objeto, propiedad) {
    // Esta propiedad será igual al valor `null`.
    // Retornar el objeto.
    // Tu código:
+   objeto[propiedad] = null;
+   return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -29,36 +34,61 @@ function invocarMetodo(objeto, metodo) {
    // Esta propiedad contiene una función en su interior. Debes invocarla/ejecutarla.
    // [NOTA]: no necesitar retornar nada.
    // Tu código:
+   objeto[metodo]();
+   
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
    // El parámetro "objetoMisterioso" posee una propiedad con el nombre "numeroMisterioso".
    // Debes multiplicar este número por 5 y retornar el resultado.
    // Tu código:
+   let resultado= objetoMisterioso.numeroMisterioso*5
+   return resultado
 }
 
 function eliminarPropiedad(objeto, propiedad) {
    // El parámetro "propiedad" es una propiedad del objeto que recibes.
    // Debes eliminarla del objeto y retornarlo finalmente.
    // Tu código:
+    
+   delete objeto[propiedad]
+   return objeto
 }
 
 function tieneEmail(objetoUsuario) {
    // Verifica si el "objetoUsuario", en su propiedad "email", posee un valor definido.
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objetoUsuario.email !== undefined){
+      return true
+   }
+   else{
+      return false
+   }
 }
 
 function tienePropiedad(objeto, propiedad) {
    // Verifica si el objeto recibido posee una propiedad con el mismo nombre que el parámetro "propiedad".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objeto.hasOwnProperty(propiedad)){
+      return true
+   }
+   else{
+      return false
+   }
 }
 
 function verificarPassword(objetoUsuario, password) {
    // Verifica si la propiedad "password" del "objetoUsuario" coincide con el parámetro "password".
    // En ese caso retornar true. Caso contrario, false.
    // Tu código:
+   if(objetoUsuario.password == password){
+      return true
+   }
+   else{
+      return false
+   }
 }
 
 function actualizarPassword(objetoUsuario, nuevaPassword) {
@@ -66,6 +96,8 @@ function actualizarPassword(objetoUsuario, nuevaPassword) {
    // La nueva contraseña la recibes por parámetro.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario.password= nuevaPassword
+   return objetoUsuario
 }
 
 function agregarAmigo(objetoUsuario, nuevoAmigo) {
@@ -73,6 +105,9 @@ function agregarAmigo(objetoUsuario, nuevoAmigo) {
    // Debes agregar el "nuevoAmigo" al final de este arreglo.
    // Retornar el objeto.
    // Tu código:
+   objetoUsuario.amigos=[];
+   objetoUsuario.amigos.push(nuevoAmigo);
+   return objetoUsuario;
 }
 
 function pasarUsuarioAPremium(objetoMuchosUsuarios) {
@@ -81,6 +116,10 @@ function pasarUsuarioAPremium(objetoMuchosUsuarios) {
    // Define esta propiedad de todos los usuarios como true.
    // Retornar el arreglo.
    // Tu código:
+   for (let i = 0; i < objetoMuchosUsuarios.length; i++) {
+      objetoMuchosUsuarios[i].esPremium = true;
+  }
+  return objetoMuchosUsuarios;
 }
 
 function sumarLikesDeUsuario(objetoUsuario) {
@@ -89,6 +128,13 @@ function sumarLikesDeUsuario(objetoUsuario) {
    // Cada post posee una propiedad llamada "likes". Esta propiedad es un número.
    // Debes sumar los likes de todos los post y retornar el resultado.
    // Tu código:
+
+   let resultado=0;
+
+   for (let i = 0; i < objetoUsuario.posts.length; i++) {
+      resultado = objetoUsuario.posts[i].likes+resultado;
+   }
+   return resultado 
 }
 
 function agregarMetodoCalculoDescuento(objetoProducto) {
@@ -102,6 +148,13 @@ function agregarMetodoCalculoDescuento(objetoProducto) {
    // PorcentajeDeDescuento ---> 0.2
    // Precio final ---> 8
    // Tu código:
+
+   objetoProducto.calcularPrecioDescuento = function(){
+      let Descuento= precio*objetoProducto.porcentajeDeDescuento;
+      let precioFinal= objetoProducto.precio-Descuento; 
+      return precioFinal
+   }
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
